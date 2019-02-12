d04941dc89fc4452bd4a00ff460267d6f56a06f9
Anurzhankyzy <47302472+Anurzhankyzy@users.noreply.github.com>
2019-02-12 09:52:52 +0600

    Update Program.cs

--- Week1/task3/task3/Program.cs
+++ Week1/task3/task3/Program.cs
@@ -11,27 +11,25 @@ namespace task3
         static void Main(string[] args)
         {
             int n;             // input array size variable
-            bool isInt = int.TryParse(Console.ReadLine(), out n);   // check the array size is integer or not, if yes, assign to variable 'n'
+            bool isInt = int.TryParse(Console.ReadLine(), out n); // check the array size is integer or not, if yes, assign to variable 'n'
 
-            if(!isInt)                            // if the line from console is not integer, then quit the program
+            if(!isInt)                    // if the line from console is not integer, then quit the program
             {
                 return;
             }
 
-            string[] a = Console.ReadLine().Split();      // 1. read the line from console, 2. create an array from the input line by splitting by space
+            string[] a = Console.ReadLine().Split();  // 1. read the line from console, 2. create an array from the input line by splitting by space
 
-            for (int i = 0; i < n; i++)              // go through the array
-            {  
-                int x;                               // current input array's element
-                isInt = int.TryParse(a[i], out x);    // check the array's element is integer or not, if yes, assign to variable 'x'
-
-                if (isInt)                           // if cuurent element is integer, then
-                {
-                    for (int j = 0; j < 2; j++)        // with cycle output the array's elements
-                    {
-                        Console.Write(a[i] + " ");     // output " "(space) after every array's element
-                    }
-                }
+            string[] answ = new string[n * 2];        // create a new array of strings, which length more 2 times than the first array
+            
+            for(int i = 0; i < n; i++)      // go through the array
+            {
+                answ[2 * i] = answ [2 * i + 1] = a[i];   // answ[0] = answ[1] = a[0]
+            }
+            
+            foreach(string i in answ)    // use a cycle 'foreach'
+            {
+                Console.Write(i + " ");   // output array's element with space(" ")
             }
             Console.ReadKey(); // hold on the console until any key is pressed
         }
