489edad9901f67ef4c61b387b16f03a71c5765e4
anurzhankyzy <asema2861@gmail.com>
2019-02-15 01:11:07 +0600

    week2 task3 and task4

---
+++ week2/Task3/Task3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task3
+{
+    class Program
+    {
+        static void PrintSp(int lvl)               // create a function which will print a space(" ") on every step
+        {
+            for (int i = 0; i < lvl; i++)          // use a cycle 'for', that print the space(" ")
+            {
+                Console.Write(" ");
+            }
+        }
+
+        static void Direction(DirectoryInfo d, int lvl)     // create a function which will return a directory
+        {
+            FileInfo[] ff = d.GetFiles();                   // create an array of files
+
+            foreach (FileInfo file in ff)                    // use a cycle 'foreach' that go through all files 
+            {
+                PrintSp(lvl);                               // firstly, print the space(" ")
+
+                Console.WriteLine(file.Name);               // then, output a file name
+            }
+
+            DirectoryInfo[] directories = d.GetDirectories();    // create an array of directories
+
+            foreach (DirectoryInfo dir in directories)           // use a cycle 'foreach'
+            {
+                PrintSp(lvl);                                   // print the space(" ")
+
+                Console.WriteLine(dir.Name);                    // output a name of directory
+
+                Direction(dir, lvl + 1);                        // call the next time a function with lvl+1 
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            DirectoryInfo pth = new DirectoryInfo("/Program Files/WindowsPowerShell");    // create directory with path 
+            Direction(pth, 0);                                         // call a function with level (lvl = 0)
+
+            Console.ReadKey();                                         // hold on a console until any key is pressed
+        }
+    }
+}
---
+++ week2/Task4/Task4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "forTarget.txt";   // create a string with file's name
+            string source = "/Users/Asem/source";   // create a string which specify a path of source folder
+            string target = "/Users/Asem/source/target";   // create a string which specify a path of target folder
+
+            string sourcefile = Path.Combine(source, name);    // use Path class to manipulate file and directory paths; create a string which combain a file name of the source folder
+            string dest = Path.Combine(target, name);          // create a string which combain a file name of the target folder
+
+            if (!Directory.Exists(target))                      // if a directory of the target folder does not exist, then 
+            {
+                Directory.CreateDirectory(target);             // creating a new directory 
+            }
+
+            File.Copy(sourcefile, dest);                       // copy the file from the source folder to the target folder 
+            File.Delete(sourcefile);                           // after copying the file to the target file, delete this file in the source folder
+
+            Console.ReadKey();                                 // hold on the console until any key is pressed  
+        }
+    }
+}
