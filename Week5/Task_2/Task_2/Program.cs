7f07c63c5d275b57cd50a45ee5c42b7db5e99d20
anurzhankyzy <asema2861@gmail.com>
2019-03-29 07:59:18 +0600

    Serialization

---
+++ Week5/Task_2/Task_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(93);
+            Point p2 = new Point(75);
+            Point p3 = new Point(64);
+
+            List<Point> list = new List<Point>() { p1, p2, p3 };
+            Point.Serialize(list);
+            List<Point> list2 = Point.Deserialize();
+
+            foreach(Point point in list2)
+            {
+                Console.WriteLine(point);
+            }
+            Console.ReadKey();
+        }
+    }
+}
